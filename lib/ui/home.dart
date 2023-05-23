4009cdfc2678b9cc84cfa8f2489d520e1f463570
kitagawaowo <uwu20201b980@gmail.com>
2023-05-23 14:55:03 -0500

    Pre transformation

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:super_zoundv2/data/Album.dart';
+import 'package:super_zoundv2/data/album.dart';
 
 class Home extends StatelessWidget {
   Home({super.key});
 
-  final albums = [Album('1', 'Parachutes', 'ColdPaly', '2002', 'https://avatars.githubusercontent.com/u/128727709?s=400&u=8fdd3a2adfe01e35d376966deafd4b769d155ee5&v=4')];
+  final albums = [Album(id: '1', name: 'Parachutes', artist: 'ColdPaly', yearReleased: '3000', urlPoster: 'https://avatars.githubusercontent.com/u/128727709?s=400&u=8fdd3a2adfe01e35d376966deafd4b769d155ee5&v=4')];
 
   @override
   Widget build(BuildContext context) {
@@ -17,17 +17,17 @@ class Home extends StatelessWidget {
         itemBuilder: (context, index) {
           return Card(
             child: ListTile(
-              title: Text(albums[index].name),
+              title: Text(albums[index].name!),
               subtitle: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(albums[index].artist),
-                  Text(albums[index].yearReleased),
+                  Text(albums[index].artist!),
+                  Text(albums[index].yearReleased!),
                 ],
               ),
               leading: Image(
                 image: NetworkImage(
-                  albums[index].urlPoster,
+                  albums[index].urlPoster!,
                 )
               )
             ),
