0990c6eb3fcacd6f82bdcdfa3f82907be7ed9790
kitagawaowo <uwu20201b980@gmail.com>
2023-05-23 15:07:13 -0500

    unstaged

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,11 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:super_zoundv2/data/album.dart';
+import 'package:super_zoundv2/data/http_helper.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   Home({super.key});
 
-  final albums = [Album(id: '1', name: 'Parachutes', artist: 'ColdPaly', yearReleased: '3000', urlPoster: 'https://avatars.githubusercontent.com/u/128727709?s=400&u=8fdd3a2adfe01e35d376966deafd4b769d155ee5&v=4')];
+  @override
+  State<Home> createState() => _HomeState();
+}
 
+class _HomeState extends State<Home> {
+  List<Album>? albums; 
+  HttpHelper? httpHelper;
+
+  @override
+  void initState() async {
+    httpHelper = HttpHelper();
+    initialize();
+    super.initState();
+  }
+
+  Future initialize() async{
+    albums = List.empty();
+    albums = await httpHelper?.getAlbums();
+  }
+  //final albums = [Album(id: '1', name: 'Parachutes', artist: 'ColdPaly', yearReleased: '3000', urlPoster: 'https://avatars.githubusercontent.com/u/128727709?s=400&u=8fdd3a2adfe01e35d376966deafd4b769d155ee5&v=4')];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
